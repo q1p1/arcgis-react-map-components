d5928db4139374669e753304a1ff07143e2f510c
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2024-07-23 06:45:01 +0300

    00007 starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { ArcgisMap } from "@arcgis/map-components-react";
 import "./App.css";
 
+// If GeoJSON files are not on the same domain as your website, a CORS enabled server
+// or a proxy is required.
+const url =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
 function App() {
   return (
     <div className="mapDiv">
