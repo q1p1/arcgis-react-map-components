ada691a22d5967773e9bcac08958264b24ecdff2
q1p1 <meshari.algahbban@wakecap.com>
2024-07-29 14:00:09 +0300

    1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { defineCustomElements as defineMapElements } from "@arcgis/map-components/dist/loader";
+defineMapElements(window, {
+  resourcesUrl: "http://js.arecgis.com/map-components/4.30/",
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
